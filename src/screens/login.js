b50facde843d8a56385fb1d4f38f03851b75447e
mviitoria113 <marcella.correa@etec.sp.gov.br>
2024-10-14 12:04:04 -0300

    Add files via upload

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,4 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from 'react';
+import {
+  Inter_700Bold,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+import {
+  DMSerifDisplay_400Regular,
+  DMSerifDisplay_400Regular_Italic,
+} from '@expo-google-fonts/dm-serif-display';
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 import { View, Image, TouchableOpacity, Text, StyleSheet, Dimensions } from "react-native";
 import { Input } from "react-native-elements";
 
@@ -21,12 +35,33 @@ export default function Login({ navigation }) {
       setError("Credenciais inválidas, tente novamente.");
     }
   };
+  const [appIsReady, setAppIsReady] = useState(false);
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          Inter_700Bold,
+          Inter_500Medium,
+          Inter_600SemiBold,
+          Inter_400Regular,
+          DMSerifDisplay_400Regular,
+          DMSerifDisplay_400Regular_Italic,
+        });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
   return (
     <View style={styles.container}>
       <Image source={require("../assets/borboleta.png")} style={styles.borboleta} />
       <View style={styles.container2}>
-        <Image source={require("../assets/titulo_login.png")} style={styles.titulo} />
+      <Text style={styles.Title}>Login</Text>
         <View style={styles.email}>
           <Input
             style={styles.input}
@@ -90,16 +125,19 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     alignItems: "center",
   },
-  titulo: {
-    marginTop: height * 0.04,
-    width: width * 0.39,
-    height: width * 0.095,
+  Title: {
+    fontSize: 65,
+    fontFamily: 'DMSerifDisplay_400Regular',
+    color: '#631C1C',
+    textAlign: 'center',
+    paddingTop: 12,
+   
   },
   email: {
     backgroundColor: "#FFFFFF",
     width: width * 0.9,
     height: height * 0.1,
-    marginTop: height * 0.04,
+    marginTop: height * 0.02,
     borderWidth: 1,
     borderColor: "#49070A",
     borderRadius: 20,
