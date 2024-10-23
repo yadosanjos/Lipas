2d93741bde49c456e67e950f7590e18282ad3cbb
mviitoria113 <marcella.correa@etec.sp.gov.br>
2024-10-23 12:19:25 -0300

    Add files via upload

--- src/routes/drawer.js
+++ src/routes/drawer.js
@@ -1,4 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import {
+  Inter_700Bold,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+import {
+  DMSerifDisplay_400Regular,
+  DMSerifDisplay_400Regular_Italic,
+} from '@expo-google-fonts/dm-serif-display';
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 import { createDrawerNavigator, StyleSheet, View, Text, Image, DrawerItem} from "@react-navigation/drawer";
 import CustomDrawer from "../components/customDrawer/input" 
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -12,6 +26,28 @@ import { Icon } from "react-native-paper";
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          Inter_700Bold,
+          Inter_500Medium,
+          Inter_600SemiBold,
+          Inter_400Regular,
+          DMSerifDisplay_400Regular,
+          DMSerifDisplay_400Regular_Italic,
+        });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
   return (
     <Drawer.Navigator 
     drawerContent={props => <CustomDrawer {...props}/>}
@@ -19,12 +55,17 @@ const DrawerNavigator = () => {
     drawerStyle: {
     backgroundColor: '#FFEDE3',
     },
+    headerTitleStyle:{
+      fontFamily: 'DMSerifDisplay_400Regular',
+      fontSize: 33,
+    },
     headerStyle: {
     backgroundColor: '#49070A',
     },
     headerTintColor: '#FFEDE3', 
     drawerActiveTintColor: '#791227',
     drawerTintColor: '#FFEDE3', 
+    
   }}>
 
     <Drawer.Screen name="Home" component={TabNavigator} options={{
