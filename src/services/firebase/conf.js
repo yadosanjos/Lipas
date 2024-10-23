4aba4bf06cd4660e0d7d2206cfe2faed37ed6745
JuliaCabrall <julia.souza350@etec.sp.gov.br>
2024-10-23 10:42:33 -0300

    Add files via upload

--- src/services/firebase/conf.js
+++ src/services/firebase/conf.js
@@ -4,6 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyAQ2HGaih-ul55zjzmIntVGRjVJ7ubnUG8",
   authDomain: "lipa-s.firebaseapp.com",
@@ -14,12 +15,15 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
-
 const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { app, auth, db, storage };
+
+
+
+
+
