12c8ad43f8ea6f6569865586e016b506ecf56e7e
mviitoria113 <marcella.correa@etec.sp.gov.br>
2024-11-06 19:18:47 -0300

    Add files via upload

--- src/components/customDrawer/input.js
+++ src/components/customDrawer/input.js
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { auth, db } from '../../services/firebase/conf';  // Certifique-se de importar corretamente seu módulo de firebase
 import { doc, getDoc } from 'firebase/firestore';
 import { useNavigation } from '@react-navigation/native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome' 
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import Entypo from 'react-native-vector-icons/Entypo'
 const CustomDrawer = (props) => {
   const [userName, setUserName] = useState('');
   const [userEmail, setUserEmail] = useState('');
   const [image, setImage] = useState('https://thumbs.dreamstime.com/b/icono-de-la-mujer-para-el-perfil-imagen-femenino-ser-humano-o-muestra-y-s%C3%ADmbolo-gente-126138203.jpg');
 const navigation = useNavigation();
+const [modalVisible, setModalVisible] = useState(false);
+
+
+const handleExit = () => {
+  // Aqui você pode adicionar a lógica de saída do app
+  console.log("Saindo do app...");
+  signOut()
+  setModalVisible(false);
+};
   useEffect(() => {
     const fetchUserData = async () => {
       const user = auth.currentUser;
@@ -63,12 +75,36 @@ const navigation = useNavigation();
         </View>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
-      <TouchableOpacity style={styles.exitButton} onPress={signOut}>
+      <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.exitButton}>
         <Text style={styles.exitButtonText}>Sair</Text>
       </TouchableOpacity>
-      <View>
-        <Text></Text>
-      </View>
+        {/* Modal de confirmação */}
+        <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setModalVisible(false)}
+        
+      >
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+          <Entypo name="log-out" size={40} color="#49070A" />
+            <Text style={styles.title}>Deseja sair do Lipa's?</Text>
+
+            <View style={styles.buttonGroup}>
+              {/* Botão "Sim" */}
+              <TouchableOpacity onPress={handleExit} style={styles.yesButton}>
+                <Text style={styles.buttonSim}>Sim</Text>
+              </TouchableOpacity>
+
+              {/* Botão "Não" */}
+              <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.noButton}>
+                <Text style={styles.buttonNo}>Não</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -115,19 +151,78 @@ exitButton: {
   borderColor: '#631C1C',
   borderWidth: 2,
   padding: 1,
-  marginHorizontal: 2,
-  borderRadius: 5,
   alignItems: 'center',
   marginTop: 55,
   marginHorizontal: 55,
   paddingVertical: 10,
   borderRadius: 30,
+  marginVertical: 23,
 },
 exitButtonText: {
   fontFamily: 'Inter_700Bold',
   color: '#631C1C',
   fontSize: 24,
   textAlign: 'center'
+},
+openButton: {
+  backgroundColor: '#631C1C',
+  paddingVertical: 15,
+  borderRadius: 30,
+  alignItems: 'center',
+  marginTop: 250,
+  marginHorizontal: 60,
+},
+openButtonText: {
+  color: '#fff',
+  fontSize: 24,
+  fontFamily: 'Inter_700Bold',
+  marginEnd: 10,
+},
+modalBackground: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+},
+modalContainer: {
+  width: '80%',
+  backgroundColor: '#FAE9E4',
+  borderRadius: 15,
+  padding: 22,
+  alignItems: 'center',
+  elevation: 10,
+  height: '31%',
+},
+title: {
+  fontSize: 25,
+  fontFamily: 'Inter_700Bold',
+  color: '#49070A',
+  marginBottom: 2,
+  textAlign: 'center',
+  borderBottomWidth: 1,
+  borderColor: '#DDC2BB',
+  paddingVertical: 20,
+  alignItems: 'center',
+  width: '115%',
+},
+buttonGroup: {
+  width: '115%',
+},
+buttonSim: {
+  color: '#631C1C',
+  fontSize: 22,
+  fontFamily: 'Inter_600SemiBold',
+  textAlign: 'center',
+  borderBottomWidth: 1,
+  borderColor: '#DDC2BB',
+  paddingVertical: 14,
+},
+buttonNo:{
+  color: '#631C1C',
+  fontSize: 22,
+  fontFamily: 'Inter_600SemiBold',
+  textAlign: 'center',
+  paddingVertical: 8,
 },
 });
 export default CustomDrawer;
