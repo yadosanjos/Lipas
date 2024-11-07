dd72bfeff76a6922402447f386acbec7faa93ab6
mviitoria113 <marcella.correa@etec.sp.gov.br>
2024-11-06 21:06:33 -0300

    Add files via upload

--- src/screens/fontsize.js
+++ src/screens/fontsize.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import Slider from '@react-native-community/slider';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const FontSizeScreen = ({navigation}) => {
   const [fontSize, setFontSize] = useState(18);
 
+  // Função para aumentar o tamanho da fonte
+  const increaseFontSize = () => {
+    if (fontSize < 30) {
+      setFontSize(fontSize + 2);
+    }
+  };
+
+  // Função para diminuir o tamanho da fonte
+  const decreaseFontSize = () => {
+    if (fontSize > 12) {
+      setFontSize(fontSize - 2);
+    }
+  };
+
   return (
     <View style={styles.container}>
-        <View style={styles.navbar}>
+      <View style={styles.navbar}>
         {/* Botão de menu à esquerda */}
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <MaterialIcons name="arrow-back-ios" size={30} color="#FFEDE3" />
@@ -30,26 +40,23 @@ const FontSizeScreen = ({navigation}) => {
           Observando o nosso cotidiano como mulheres, e vendo que na atual realidade brasileira, conclusões nos dizem que diversas complicações na segurança ditam as dificuldades que estas inseridas, assim identificamos a necessidade de interferência, que ateste a nossa liberdade, diante deste cenário o nosso aplicativo tem como objetivo trazer a automação para que nos gere feedbacks e fazendo aplicações que trazem a sensação de segurança imediata.
         </Text>
       </View>
-     
-      <View style={styles.sliderLab}>
-      <Slider
-        style={styles.slider}
-        minimumValue={12}
-        maximumValue={30}
-        value={fontSize}
-        onValueChange={(value) => setFontSize(value)}
-        minimumTrackTintColor="#A52A2A"
-        maximumTrackTintColor="#000000"
-      />
-       </View>
-       <View style={styles.sliderLabels}>
-        <Text style={styles.sliderLabel}>-A</Text>
-        <Text style={styles.previewText}>Pré-visualização</Text>
-        <Text style={styles.sliderLabelLarge}>+A</Text>
-        </View>
+
+      <Text style={styles.previewText}>Pré-visualização</Text>
+
+      {/* Contêiner para os botões */}
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={decreaseFontSize}>
+          <Text style={styles.buttonText}>-A</Text>
+        </TouchableOpacity>
+        <Text style={styles.Text}>Pré-Visualização</Text>
+        <TouchableOpacity style={styles.button} onPress={increaseFontSize}>
+          <Text style={styles.buttonText}>+A</Text>
+        </TouchableOpacity>
       </View>
+    </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -57,7 +64,7 @@ const styles = StyleSheet.create({
   },
   sampleText: {
     color: '#FFEDE3',
-    marginStart: 15,
+    marginStart: 20,
     marginVertical: 20,
   },
   previewText: {
@@ -65,49 +72,38 @@ const styles = StyleSheet.create({
     color: '#49070A',
     textAlign: 'center',
     marginVertical: 30,
-    color: '#49070A',
     fontFamily: 'Inter_500Medium',
   },
-  sliderLabels: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 16,
-    alignItems: 'center',
-    width: '100%',
-    borderRadius: 30,
-    padding: 2,
-    height: '40%',
-    marginVertical: 670,
+  // Estilo do contêiner dos botões
+  buttonContainer: {
     position: 'absolute',
-  },
-  sliderLab: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 1,
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     width: '100%',
+    height: 150,
     backgroundColor: '#FAE9E4',
-    borderRadius: 30,
-    height: '40%',
-    marginVertical: 620,
-    position: 'absolute',
+    paddingVertical: 10,
+    borderRadius: 20,
+    alignSelf: 'center', // Centraliza o contêiner na tela
+    marginTop: 755,
   },
-  sliderLabel: {
+  button: {
+    width: 60,
+    height: 40,
+    backgroundColor: '#F3EDE1',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
     fontSize: 18,
-    textAlign: 'center',
-      fontFamily: 'Inter_500Medium',
-    color: '#49070A',
-    
-  },
-  sliderLabelLarge: {
-    fontSize: 24,
-    textAlign: 'center',
-    fontFamily: 'Inter_400Regular',
-    color: '#49070A',
+    fontFamily: 'Inter_500Medium',
+    color:'#49070A',
   },
   navbar: {
-    width: '100%', // Faz a navbar ocupar toda a largura
-    height: 75, // Ajuste a altura se necessário
+    width: '100%',
+    height: 75,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
@@ -119,13 +115,18 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 35,
     color: '#FFEDE3',
-    fontFamily: 'DMSerifDisplay_400Regular', // Certifique-se de carregar a fonte correta
+    fontFamily: 'DMSerifDisplay_400Regular',
   },
-   borboleta: {
+  borboleta: {
     width: 55,
     height: 50,
     marginStart: 19,
     marginVertical: 13,
+  },
+  Text:{
+    fontFamily: 'Inter_500Medium',
+    fontSize: 18,
+    color: '#49070A',
   },
 });
 
