e63ca33f0d34228b92245bef676407ffc47a4bd9
Ya <yadosanjoss@gmail.com>
2024-12-03 16:45:53 -0300

    Add files via upload

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert } from 'react-native';
+import { View, Text, StyleSheet, Alert, Image, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';
 import MapView, { Marker } from 'react-native-maps';
+
+import { db, auth } from '../services/firebase/conf';
+import { onAuthStateChanged } from 'firebase/auth';
+import { collection, query, where, onSnapshot, doc, getDoc, deleteDoc } from 'firebase/firestore';
 
 const HomeScreen = () => {
   const [region, setRegion] = useState(null);
   const [userLocation, setUserLocation] = useState(null);
+  const [userEmail, setEmail] = useState('');
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     // Solicita permissões para acessar a localização
@@ -43,6 +49,38 @@ const HomeScreen = () => {
     }
   }
 
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const docRef = doc(db, "Usuário", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const fetchedUserEmail = docSnap.data().userEmail;
+          setEmail(fetchedUserEmail);
+        }
+      }
+    });
+
+    return () => unsubscribeAuth();
+  }, []);
+
+  useEffect(() => {
+    if (userEmail) {
+      const contactsRef = collection(db, 'Contato de Emergência');
+      const q = query(contactsRef, where('user', '==', userEmail));
+
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const contactsData = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setContacts(contactsData);
+      });
+
+      return () => unsubscribe();
+    }
+  }, [userEmail]);
+
   return (
     <View style={styles.container}>
       {region && userLocation ? (
@@ -74,6 +112,27 @@ const HomeScreen = () => {
         ) : (
           <Text style={styles.locationText}>Buscando localização...</Text>
         )}
+
+      {contacts.map(contact => (
+          <View key={contact.id} style={styles.contactContainer}>
+            <View style={styles.alinhamento}>
+              <Text style={styles.contactName}>{contact.nomeContato || 'Nome não disponível'}</Text>
+            {contact.type === 'lipas' && (
+              <Image source={require('../assets/borbo.png')} style={styles.iconButterfly} />
+            )}
+            </View>
+            <View style={styles.alinhamento2}>
+              <Text style={styles.contactEmail}>{contact.emailContato}</Text>
+            </View>
+            <View style={styles.alinhamento3}>
+               <Text style={styles.contactCelular}>{contact.celularContato}</Text>
+            </View>
+            <TouchableOpacity onPress={() => removeContact(contact.id)} style={styles.deleteButton}>
+              <Text style={styles.deleteText}> Excluir </Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+
       </View>
     </View>
   );
